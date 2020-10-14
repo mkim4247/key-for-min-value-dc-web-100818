5cbad22034e5a3ede6dd6558abaea2b03c9f6d7d
Michael Kim <m.kim4247@gmail.com>
2020-10-14 02:24:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_val = nil
   name_hash do |key, value|
-    if min_key == nil || value < min_val
+    if min_val == nil || value < min_val
       min_key = key
       min_val = value
     end
