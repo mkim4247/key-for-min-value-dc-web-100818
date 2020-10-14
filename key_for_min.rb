1f2b3c987a3ebd3b77b9b4e00d6a9ad30af6eeaf
Michael Kim <m.kim4247@gmail.com>
2020-10-14 02:08:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_key = name_hash.first[0]
+  min_val = name_hash.first[1]
   if name_hash != {}
-    
+    name_hash.collect do |key, value|
+      
   else
     nil
+  end
 end
 
 
